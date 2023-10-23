b5bd01b937e1323b3e18b5d86c0f82c6642d54d1
DevExpressExampleBot <fakeEmail>
2023-10-23 11:50:54 +0400

    Source auto update [skip ci]

--- VB/DocumentServer_PrintToPDF/Form1.Designer.vb
+++ VB/DocumentServer_PrintToPDF/Form1.Designer.vb
@@ -1,112 +1,114 @@
-Imports Microsoft.VisualBasic
-Imports System
 Namespace DocumentServerExample
-	Partial Public Class Form1
-		''' <summary>
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+    Partial Class Form1
 
-		#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Required method for Designer support - do not modify
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
-			Me.components = New System.ComponentModel.Container()
-			Me.btnConvert = New System.Windows.Forms.Button()
-			Me.timer1 = New System.Windows.Forms.Timer(Me.components)
-			Me.textBox1 = New System.Windows.Forms.TextBox()
-			Me.label1 = New System.Windows.Forms.Label()
-			Me.label2 = New System.Windows.Forms.Label()
-			Me.textBox2 = New System.Windows.Forms.TextBox()
-			Me.SuspendLayout()
-			' 
-			' btnConvert
-			' 
-			Me.btnConvert.Location = New System.Drawing.Point(15, 244)
-			Me.btnConvert.Name = "btnConvert"
-			Me.btnConvert.Size = New System.Drawing.Size(75, 23)
-			Me.btnConvert.TabIndex = 0
-			Me.btnConvert.Text = "Start!"
-			Me.btnConvert.UseVisualStyleBackColor = True
-'			Me.btnConvert.Click += New System.EventHandler(Me.btnConvert_Click);
-			' 
-			' timer1
-			' 
-			Me.timer1.Interval = 5000
-			' 
-			' textBox1
-			' 
-			Me.textBox1.Location = New System.Drawing.Point(12, 29)
-			Me.textBox1.Name = "textBox1"
-			Me.textBox1.Size = New System.Drawing.Size(448, 20)
-			Me.textBox1.TabIndex = 1
-			' 
-			' label1
-			' 
-			Me.label1.AutoSize = True
-			Me.label1.Location = New System.Drawing.Point(147, 249)
-			Me.label1.Name = "label1"
-			Me.label1.Size = New System.Drawing.Size(0, 13)
-			Me.label1.TabIndex = 2
-			' 
-			' label2
-			' 
-			Me.label2.AutoSize = True
-			Me.label2.Location = New System.Drawing.Point(12, 9)
-			Me.label2.Name = "label2"
-			Me.label2.Size = New System.Drawing.Size(84, 13)
-			Me.label2.TabIndex = 3
-			Me.label2.Text = "Document Path:"
-			' 
-			' textBox2
-			' 
-			Me.textBox2.AcceptsReturn = True
-			Me.textBox2.Location = New System.Drawing.Point(15, 62)
-			Me.textBox2.Multiline = True
-			Me.textBox2.Name = "textBox2"
-			Me.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
-			Me.textBox2.Size = New System.Drawing.Size(445, 156)
-			Me.textBox2.TabIndex = 4
-			' 
-			' Form1
-			' 
-			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
-			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-			Me.ClientSize = New System.Drawing.Size(569, 279)
-			Me.Controls.Add(Me.textBox2)
-			Me.Controls.Add(Me.label2)
-			Me.Controls.Add(Me.label1)
-			Me.Controls.Add(Me.textBox1)
-			Me.Controls.Add(Me.btnConvert)
-			Me.Name = "Form1"
-			Me.Text = "Automatic Document to PDF Converter"
-			Me.ResumeLayout(False)
-			Me.PerformLayout()
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
+            End If
 
-		End Sub
+            MyBase.Dispose(disposing)
+        End Sub
 
-		#End Region
+#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.components = New System.ComponentModel.Container()
+            Me.btnConvert = New System.Windows.Forms.Button()
+            Me.timer1 = New System.Windows.Forms.Timer(Me.components)
+            Me.textBox1 = New System.Windows.Forms.TextBox()
+            Me.label1 = New System.Windows.Forms.Label()
+            Me.label2 = New System.Windows.Forms.Label()
+            Me.textBox2 = New System.Windows.Forms.TextBox()
+            Me.SuspendLayout()
+            ' 
+            ' btnConvert
+            ' 
+            Me.btnConvert.Location = New System.Drawing.Point(15, 244)
+            Me.btnConvert.Name = "btnConvert"
+            Me.btnConvert.Size = New System.Drawing.Size(75, 23)
+            Me.btnConvert.TabIndex = 0
+            Me.btnConvert.Text = "Start!"
+            Me.btnConvert.UseVisualStyleBackColor = True
+            AddHandler Me.btnConvert.Click, New System.EventHandler(AddressOf Me.btnConvert_Click)
+            ' 
+            ' timer1
+            ' 
+            Me.timer1.Interval = 5000
+            ' 
+            ' textBox1
+            ' 
+            Me.textBox1.Location = New System.Drawing.Point(12, 29)
+            Me.textBox1.Name = "textBox1"
+            Me.textBox1.Size = New System.Drawing.Size(448, 20)
+            Me.textBox1.TabIndex = 1
+            ' 
+            ' label1
+            ' 
+            Me.label1.AutoSize = True
+            Me.label1.Location = New System.Drawing.Point(147, 249)
+            Me.label1.Name = "label1"
+            Me.label1.Size = New System.Drawing.Size(0, 13)
+            Me.label1.TabIndex = 2
+            ' 
+            ' label2
+            ' 
+            Me.label2.AutoSize = True
+            Me.label2.Location = New System.Drawing.Point(12, 9)
+            Me.label2.Name = "label2"
+            Me.label2.Size = New System.Drawing.Size(84, 13)
+            Me.label2.TabIndex = 3
+            Me.label2.Text = "Document Path:"
+            ' 
+            ' textBox2
+            ' 
+            Me.textBox2.AcceptsReturn = True
+            Me.textBox2.Location = New System.Drawing.Point(15, 62)
+            Me.textBox2.Multiline = True
+            Me.textBox2.Name = "textBox2"
+            Me.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+            Me.textBox2.Size = New System.Drawing.Size(445, 156)
+            Me.textBox2.TabIndex = 4
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(569, 279)
+            Me.Controls.Add(Me.textBox2)
+            Me.Controls.Add(Me.label2)
+            Me.Controls.Add(Me.label1)
+            Me.Controls.Add(Me.textBox1)
+            Me.Controls.Add(Me.btnConvert)
+            Me.Name = "Form1"
+            Me.Text = "Automatic Document to PDF Converter"
+            Me.ResumeLayout(False)
+            Me.PerformLayout()
+        End Sub
 
-		Private WithEvents btnConvert As System.Windows.Forms.Button
-		Private timer1 As System.Windows.Forms.Timer
-		Private textBox1 As System.Windows.Forms.TextBox
-		Private label1 As System.Windows.Forms.Label
-		Private label2 As System.Windows.Forms.Label
-		Private textBox2 As System.Windows.Forms.TextBox
-	End Class
+#End Region
+        Private btnConvert As System.Windows.Forms.Button
+
+        Private timer1 As System.Windows.Forms.Timer
+
+        Private textBox1 As System.Windows.Forms.TextBox
+
+        Private label1 As System.Windows.Forms.Label
+
+        Private label2 As System.Windows.Forms.Label
+
+        Private textBox2 As System.Windows.Forms.TextBox
+    End Class
 End Namespace
-
--- VB/DocumentServer_PrintToPDF/Form1.vb
+++ VB/DocumentServer_PrintToPDF/Form1.vb
@@ -1,84 +1,83 @@
-Imports Microsoft.VisualBasic
 Imports System
 Imports System.Collections.Generic
 Imports System.ComponentModel
-Imports System.Data
 Imports System.Diagnostics
 Imports System.Drawing
 Imports System.IO
-Imports System.Text
 Imports System.Windows.Forms
 Imports DevExpress.XtraRichEdit
 
 Namespace DocumentServerExample
-	Partial Public Class Form1
-		Inherits Form
-		Private fileList As New List(Of String)()
-		Private server As RichEditDocumentServer
-		Private counter As PerformanceCounter
 
-		Public Sub New()
-			InitializeComponent()
-			server = New RichEditDocumentServer()
+    Public Partial Class Form1
+        Inherits Form
 
-			textBox1.Text = Application.StartupPath
-			AddHandler timer1.Tick, AddressOf timer1_Tick
+        Private fileList As List(Of String) = New List(Of String)()
 
-			Dim procName As String = Process.GetCurrentProcess().ProcessName
-			counter = New PerformanceCounter("Process", "Working Set - Private", procName)
-			ShowMemoryUsage()
-		End Sub
+        Private server As RichEditDocumentServer
 
-		Private Sub PrintToPDF(ByVal filePath As String)
-			Dim displayText As String
+        Private counter As PerformanceCounter
 
-			Try
-				server.LoadDocument(filePath)
-			Catch ex As Exception
-				server.CreateNewDocument()
-				displayText = String.Format("{0:T} Error:{1} -> {2}", DateTime.Now, ex.Message, filePath) & Environment.NewLine
-				textBox2.Text += displayText
-				Return
-			End Try
-			Dim outFileName As String = Path.ChangeExtension(filePath, "pdf")
-			Dim fsOut As FileStream = File.Open(outFileName, FileMode.Create)
-			server.ExportToPdf(fsOut)
-			fsOut.Close()
-			displayText = String.Format("{0:T} Done-> {1}",DateTime.Now, outFileName) & Environment.NewLine
-			textBox2.Text += displayText
-		End Sub
+        Public Sub New()
+            InitializeComponent()
+            server = New RichEditDocumentServer()
+            textBox1.Text = Application.StartupPath
+            AddHandler timer1.Tick, New EventHandler(AddressOf timer1_Tick)
+            Dim procName As String = Process.GetCurrentProcess().ProcessName
+            counter = New PerformanceCounter("Process", "Working Set - Private", procName)
+            ShowMemoryUsage()
+        End Sub
 
-		Private Sub UpdateFileList(ByVal path As String)
-			If Directory.Exists(path) Then
-				Dim files() As String = System.IO.Directory.GetFiles(path, "*.doc?", System.IO.SearchOption.AllDirectories)
+        Private Sub PrintToPDF(ByVal filePath As String)
+            Dim displayText As String
+            Try
+                server.LoadDocument(filePath)
+            Catch ex As Exception
+                server.CreateNewDocument()
+                displayText = String.Format("{0:T} Error:{1} -> {2}", Date.Now, ex.Message, filePath) & Environment.NewLine
+                textBox2.Text += displayText
+                Return
+            End Try
 
-				For Each file As String In files
-					If (Not fileList.Contains(file)) Then
-						fileList.Add(file)
-						PrintToPDF(file)
-					End If
-				Next file
-			End If
-		End Sub
+            Dim outFileName As String = Path.ChangeExtension(filePath, "pdf")
+            Dim fsOut As FileStream = File.Open(outFileName, FileMode.Create)
+            server.ExportToPdf(fsOut)
+            fsOut.Close()
+            displayText = String.Format("{0:T} Done-> {1}", Date.Now, outFileName) & Environment.NewLine
+            textBox2.Text += displayText
+        End Sub
 
-		Private Sub btnConvert_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnConvert.Click
-			If timer1.Enabled Then
-				timer1.Stop()
-				btnConvert.Text = "Start!"
-				textBox1.ReadOnly = False
-			Else
-				timer1.Start()
-				btnConvert.Text = "Stop!"
-				textBox1.ReadOnly = True
-			End If
-		End Sub
+        Private Sub UpdateFileList(ByVal path As String)
+            If Directory.Exists(path) Then
+                Dim files As String() = Directory.GetFiles(path, "*.doc?", SearchOption.AllDirectories)
+                For Each file As String In files
+                    If Not fileList.Contains(file) Then
+                        fileList.Add(file)
+                        PrintToPDF(file)
+                    End If
+                Next
+            End If
+        End Sub
 
-		Private Sub timer1_Tick(ByVal sender As Object, ByVal e As EventArgs)
-			UpdateFileList(textBox1.Text)
-			ShowMemoryUsage()
-		End Sub
-		Private Sub ShowMemoryUsage()
-			label1.Text = String.Format("Memory usage: {0:N0} K", counter.RawValue/1024)
-		End Sub
-	End Class
+        Private Sub btnConvert_Click(ByVal sender As Object, ByVal e As EventArgs)
+            If timer1.Enabled Then
+                timer1.Stop()
+                btnConvert.Text = "Start!"
+                textBox1.ReadOnly = False
+            Else
+                timer1.Start()
+                btnConvert.Text = "Stop!"
+                textBox1.ReadOnly = True
+            End If
+        End Sub
+
+        Private Sub timer1_Tick(ByVal sender As Object, ByVal e As EventArgs)
+            UpdateFileList(textBox1.Text)
+            ShowMemoryUsage()
+        End Sub
+
+        Private Sub ShowMemoryUsage()
+            label1.Text = String.Format("Memory usage: {0:N0} K", counter.RawValue / 1024)
+        End Sub
+    End Class
 End Namespace
